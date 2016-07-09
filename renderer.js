29690abbf62c1cf0a0d882edbfc59b91e68814cd
aman_d1996 <aman_d1996@yahoo.com>
2016-07-10 02:35:19 +0530

    latest code

--- renderer.js
+++ renderer.js
@@ -5,6 +5,13 @@ $('#Go').click(function(){
 	var url = $("#urlinput").val();
 	url = "http://" + url;
 	$("#webviewpane").attr('src', url);
+	$('#loadgif').show();
+});
+
+$('#homeButton').click(function(){
+	$("#webviewpane").attr('src', "http://www.google.com");
+	$("#urlinput").val("www.google.com");
+	$('#loadgif').show();
 });
 
 $('#urlinput').keypress(function (e) {
@@ -16,7 +23,7 @@ $('#urlinput').keypress(function (e) {
 });
 
 $('#addNewButton').click(function(){
-	$('#newTab').append('<button class="btn btn-md btn-primary" id="btn-" style="float:left;">New Tab</button>');
+	$('#newTab').append('<button class="btn btn-md btn-info" id="btn-" style="float:left;">New Tab</button>');
 	$('#newWebview').append('<webview src="http://www.google.com" id="webviewpane" style="height:590px; display:none;"></webview>');
 });
 
